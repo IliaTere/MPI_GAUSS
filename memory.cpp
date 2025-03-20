72105f5f492545a191322f8149238a3f31e82d79
iliatere <ilia.tereshenko@math.msu.ru>
2025-03-20 16:48:37 +0300

    WARNING MEMORY

--- memory.cpp
+++ memory.cpp
@@ -5,54 +5,47 @@ void freeAllMemory(double *matrix, double *invertedMatrix, double *blockStringBu
                    double *lmBlock1, double *llBlock, double *llBlock1, double *llBlock2, int *indicesTable,
                    double *minInvertedNormList, double *minInvNormIndexTriple, int *invertedStatus)
 {
-    if (matrix != nullptr)
-        delete [] matrix;
-
-    if (invertedMatrix != nullptr)
-        delete [] invertedMatrix;
-
-    if (invertedStatus != nullptr)
-        delete [] invertedStatus;
-
-    if (blockRow != nullptr)
-        delete [] blockRow;
-
-    if (mlBlock != nullptr)
-        delete [] mlBlock;
-
-    if (lmBlock != nullptr)
-        delete [] lmBlock;
-
-    if (lmBlock1 != nullptr)
-        delete [] lmBlock1;
-
-    if (llBlock != nullptr)
-        delete [] llBlock;
-
-    if (llBlock1 != nullptr)
-        delete [] llBlock1;
-
-    if (llBlock2 != nullptr)
-        delete [] llBlock2;
-
-    if (blockStringBuf != nullptr)
-        delete [] blockStringBuf;
-
-    if (block1 != nullptr)
-        delete [] block1;
-
-    if (block2 != nullptr)
-        delete [] block2;
-
-    if (block3 != nullptr)
-        delete [] block3;
-
-    if (indicesTable != nullptr)
-        delete [] indicesTable;
-
-    if (minInvertedNormList != nullptr)
-        delete [] minInvertedNormList;
-
-    if (minInvNormIndexTriple != nullptr)
-        delete [] minInvNormIndexTriple;
+    // Helper function to safely delete any pointer
+    auto safeDelete = [](double* &ptr) {
+        if (ptr) {
+            delete[] ptr;
+            ptr = nullptr;
+        }
+    };
+    
+    auto safeDeleteInt = [](int* &ptr) {
+        if (ptr) {
+            delete[] ptr;
+            ptr = nullptr;
+        }
+    };
+    
+    // Free matrix data structures
+    safeDelete(matrix);
+    safeDelete(invertedMatrix);
+    safeDelete(blockStringBuf);
+    
+    // Free block buffers
+    safeDelete(blockRow);
+    safeDelete(block1);
+    safeDelete(block2);
+    safeDelete(block3);
+    
+    // Free mixed-size blocks
+    safeDelete(mlBlock);
+    safeDelete(lmBlock);
+    safeDelete(lmBlock1);
+    
+    // Free lower blocks
+    safeDelete(llBlock);
+    safeDelete(llBlock1);
+    safeDelete(llBlock2);
+    
+    // Free auxiliary arrays
+    safeDeleteInt(indicesTable);
+    safeDeleteInt(invertedStatus);
+    
+    // Free norm data
+    safeDelete(minInvertedNormList);
+    safeDelete(minInvNormIndexTriple);
 } 
